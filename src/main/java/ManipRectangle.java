c3c039abb1d891b99e70fbc4169ea2efc95fa187
laurent raulier <laurent.raulier.onCoursera@gmail.com>
2021-08-08 20:27:06 +0200

    add class & method surface to Rectangle

--- src/main/java/ManipRectangle.java
+++ src/main/java/ManipRectangle.java
@@ -1,8 +1,21 @@
+class Rectangle{
+	private double largeur;
+	private double longueur;
+	
+	public Rectangle(double largeur,double longueur) {
+		this.largeur = largeur;
+		this.longueur = longueur;
+	}
+	
+	public double surface() {
+		return largeur * longueur;
+	}
+}
 
-public class ManipRectangle {
+class ManipRectangle {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("surface : " + new Rectangle(4, 7).surface());
 	}
 
 }
